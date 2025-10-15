ffd862dd72158df05814348d60c5a9ad004743ea
youben11 <ayoub.benaissa@zama.ai>
2025-10-15 12:23:17 +0100

    perf(cvm/attestation): replication at proc vs orch level
    options to switch between replication at process level or at the
    container orchestrator level

--- cvm/attestation-service/entrypoint.sh
+++ cvm/attestation-service/entrypoint.sh
@@ -3,7 +3,8 @@ set -e
 
 HOST=${HOST:-0.0.0.0}
 PORT=${PORT:-8080}
-WORKERS=${WORKERS:-4}
+# Keep it to 1 if you want replication at the container orchestration level
+WORKERS=${WORKERS:-1}
 
 echo "Starting Attestation Service..."
 echo "Host: ${HOST}"
